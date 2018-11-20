aca0f5d5a7b710cde76cf546bdc8095f89c96fb4
Carter Nesbitt <ccnesbitt@gmail.com>
2018-11-20 01:08:08 -0500

    /q now gets the most popular quote. File containing quotes still needs to be updated

--- QOTC.py
+++ QOTC.py
@@ -1,7 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import random
 import os
-import key_file
 
 quotes_name = "quotes.txt"
 count_name = "counts.txt"
@@ -17,69 +16,108 @@ def init():
     global count
     global users
     global photo_uploads
+    quotes_no_key = []
     file = open(quotes_name)
     for line in file:
-        key_val = line.strip().split(',', 1)  # splits at first comma only
-        quotes[key_val[0]] = [key_val[1], key_val[2]]
+        key_val = line.strip().split(',', 2)  # splits at first two commas  only
+        if key_val[0].strip() != "":
+            quotes[key_val[0].strip()] = [key_val[1].strip(), key_val[2].strip()]
+        else:
+            quotes_no_key.append([key_val[1].strip(), key_val[2].strip()])
     file.close()
+    quotes[None] = quotes_no_key
     file = open(count_name)
     count = int(file.readline())
     file.close()
     for line in open(photo_uploads):
         line = line.strip().split(" ")
-        users[line[0]] = [line[1]]
-    
+        users[line[0]] = line[1]
+
 def q(bot, update):
     global quotes
     best_quote = ""
     max_occurance = 0
     ref_key = ""
+    no_key_index = None
     for key in list(quotes.keys()):
-        quote = quotes[key]
-        if quote[0] > max_occurance:
-            best_quote = quote[1]
-            max_occurance = quote[0]
-            ref_key = key
-    quotes[key][0] = str(int(quotes[key][0]) + 1)
+        if key == None:  # contains all quotes with no key
+            for i in range(len(quotes[key])):  # iterate through no-key quotes
+                if int(quotes[key][i][0]) > max_occurance:
+                    best_quote = quotes[key][i][1]
+                    max_occurance = int(quotes[key][i][0])
+                    ref_key = key
+                    no_key_index = i
+        else:
+            quote = quotes[key]
+            if int(quote[0]) > max_occurance:
+                best_quote = quote[1]
+                max_occurance = int(quote[0])
+                ref_key = key
+    if ref_key is None:
+        quotes[ref_key][no_key_index][0] = str(int(quotes[ref_key][no_key_index][0]) + 1)
+    else:
+        quotes[ref_key][0]  = str(int(quotes[ref_key][0]) + 1)
     bot.send_message(chat_id=update.message.chat_id, text=best_quote)
 
 
 def r(bot, update, args):
     global quotes
+    total_entries = len(quotes) + len(quotes[None]) - 1
     if len(args) == 0:
-        quote_info_list = random.choice(list(quotes.values()))  # occurance at index 0, quote at index 1
+        choice_from_total = random.randint(0, total_entries)
+        if choice_from_total < len(quotes):  # choose from quotes with key
+            quote_info_list = [[]]  # make following conditional true
+            while type(quote_info_list[0]) == list:  # anything besides quotes[None]
+                 quote_info_list = random.choice(list(quotes.values()))  # occurance at index 0, quote at index 1
+        else:
+            quote_info_list = random.choice(quotes[None])
         q = quote_info_list[1]
+        bot.send_message(chat_id=update.message.chat_id, text=q)
+        quote_info_list[0] = str(int(quote_info_list[0]) + 1)
     else:
         user_text = " ".join(args)
-        if user_text.strip() in quotes:  
+        if user_text.strip() in quotes:
             q = quotes[user_text][1]
+            bot.send_message(chat_id=update.message.chat_id, text=q)
             quotes[user_text][0] = str(int(quotes[user_text][0]) + 1)
         else:
-            q = "Sorry, I don't seem to have any quotes for that one. Be the first to add one on that topic!"
-    bot.send_message(chat_id=update.message.chat_id, text=q)
+            filt = [i for i in quotes[None] if args[0].lower() in i[1].lower()]
+            if len(filt) == 0:
+                q = "Sorry, I don't seem to have any quotes for that one. Be the first to add one on that topic!"
+            else:
+                rand_idx = random.randint(0, len(filt)-1)
+                q = filt[rand_idx][1]
+                filt[rand_idx][0] = str(int(filt[rand_idx][0]) + 1)
+            bot.send_message(chat_id=update.message.chat_id, text=q)
     
 
 def add(bot, update, args):
     global quotes, quotes_name
-    if update.message.reply_to_message != None:
-        quote_key = " ".join(args).split()
-        if len(quote_key) == 0:
-            q = "Please retry! Make sure to give a key for the quote!"
-        else:
-            reply_text = update.message.reply_to_message.text
-            if len(reply_text.strip()) != 0:
-                quotes[quote_key] = [0, reply_text]
-                with open(quotes_name, 'a') as file:
-                    file.write(quote_key, ',', str(0), ',', reply_text, '\n')
     user_text = " ".join(args)
     key_val = user_text.split(',', 1)
+    if update.message.reply_to_message != None:
+        quote_key = user_text.split()
+        reply_text = update.message.reply_to_message.text
+        if len(reply_text.strip()) != 0:
+            if len(quote_key) == 0:
+                quotes[None].append[0, reply_text]
+            else:
+                quotes[user_text] = [0, reply_text]
+            with open(quotes_name, 'a') as file:
+                file.write(user_text + ',' + str(0) + ',' + reply_text + '\n')
     elif len(key_val) == 2:
-        quotes[key_val[0].strip()] = [0, key_val[1].strip()]
-        with open(quotes_name, 'a') as file:
-                file.write(key_val[0], ',', str(0), ',', user_text + '\n')
-    elif user_text.split('+') == 1:  # no ',' or '+' in args
-            q = "Sorry, try entering the quote like this: <key> , <quote>"
+        if key_val[0].strip() in quotes:
+            q = "Sorry, that key is already in use!"
             bot.send_message(chat_id=update.message.chat_id, text=q)
+        else:
+            quotes[key_val[0].strip()] = [0, key_val[1].strip()]
+            with open(quotes_name, 'a') as file:
+                file.write(key_val[0].strip() + ',' + str(0) + ',' + key_val[1].strip() + '\n')
+    elif len(user_text.split('+')) == 1:  # no ',' or '+' in args
+        if len(user_text.strip()) != 0:
+            quotes[None].append([0, user_text])
+            with open(quotes_name, 'a') as file:
+                file.write(',' + str(0) + ',' + "\n")
     else:
         add_lst = user_text.split('+')
         if len(add_lst) >= 2:
@@ -92,7 +130,6 @@ def add(bot, update, args):
             except ValueError:
                 q = "Invalid addition!"
                 bot.send_message(chat_id=update.message.chat_id, text=q)
-
       
 
 def say(bot, update, args):
@@ -148,7 +185,7 @@ def help(bot, update):
     update.message.reply_text("Type in '/' for a list of commands!")
 
 def main():
-    key = key_file.get_key()
+    key = os.environ['TELEGRAM_BOT_KEY']
     init()
     updater = Updater(key)
     command = updater.dispatcher
